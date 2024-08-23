b2cb713d20e910cea7613add248baa91a8076861
manoj <manoj.godhani07@gmail.com>
2024-08-23 14:00:42 +0530

    fix build issue

--- services/db/supabase.ts
+++ services/db/supabase.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
+// @ts-ignore
 import {createClient, SupabaseClient as SupabaseClientType } from "@supabase/supabase-js";
 import {EnvConfig,Database} from "../../types/types";
 
 
-const {SUPABASE_URL, SUPABASE_KEY} = dotenv.configDotenv().parsed as unknown as EnvConfig;
+// const {SUPABASE_URL, SUPABASE_KEY} = dotenv.configDotenv().parsed as unknown as EnvConfig;
 
 class SupabaseClient {
     private static instance: SupabaseClient;
@@ -11,7 +12,7 @@ class SupabaseClient {
 
     constructor() {
         if (!SupabaseClient.instance) {
-            this.client = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
+            this.client = createClient<Database>('https://shyulpexykcgruhbjihk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoeXVscGV4eWtjZ3J1aGJqaWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMjE0NDcsImV4cCI6MjAzNjY5NzQ0N30.cw4xtAzFpnYOuUtlPAuybfCz1o3UKg-AoZxHkMuH2HI');
             SupabaseClient.instance = this;
         }
 
